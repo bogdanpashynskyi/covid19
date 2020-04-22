e9c0a651c16b4968faf18fe9abd68c5eb09a4964
Bogdan Pashynskyi <bogdanpashynskyi@Bogdans-MacBook-Pro-2.local>
2020-04-23 00:44:46 +0300

    Updates

---
+++ precache-manifest.224a817bf8b33ca90684dd21294996ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9eeabf4eb8723477e28aeb5cebef69",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "c7f78ffb7c36afcb1b43",
+    "url": "/covid19/static/css/main.3a0bbb6e.chunk.css"
+  },
+  {
+    "revision": "f7db3bc9c9d570781a86",
+    "url": "/covid19/static/js/2.bff44344.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/covid19/static/js/2.bff44344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7f78ffb7c36afcb1b43",
+    "url": "/covid19/static/js/main.88e9920f.chunk.js"
+  },
+  {
+    "revision": "cdfcea41dfab553d9d0f",
+    "url": "/covid19/static/js/runtime-main.9f6017dd.js"
+  }
+]);
